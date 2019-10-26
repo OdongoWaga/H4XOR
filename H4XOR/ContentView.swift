616449ecd7348bb364ac8b0124e2b48c927a5dd0
Waga <wagaodongo@gmail.com>
2019-10-26 09:16:24 +0300

    List view

--- H4XOR/ContentView.swift
+++ H4XOR/ContentView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView {
+            List(posts) {
+                post in
+                Text(post.title)
+                }
+        
+    
+        .navigationBarTitle("H4XOR")
+    }
     }
 }
 
@@ -19,3 +28,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title:"Hola")
+]
